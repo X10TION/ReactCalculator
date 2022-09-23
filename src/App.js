13f94b2ddd39b21e9d4d3fc01662508daf3562b8
Christopher White <x0tion007@gmail.com>
2022-09-23 12:53:09 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,121 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState, useEffect} from 'react'
+import { NumericFormat } from 'react-number-format';
+
 
 function App() {
+
+  //  use state here 
+const [ preState, setPreState] = useState("")
+const [ curState, setcurState] = useState("")
+const [ input, setInput] = useState("0")
+const [operator, setOperator] = useState(null)
+const [ total, setTotal] = useState(false)
+
+
+const inputNum = (e)=> {
+  if(curState.includes(".") && e.target.innerText === ".") return
+  if(total){
+    setPreState("")
+  }
+  curState ? setcurState((pre) => 
+  pre + e.target.innerText) :
+   setcurState(e.target.innerText)
+   setTotal(false)
+}
+useEffect(() => {
+    setInput(curState)
+},[curState])
+
+useEffect(() => {
+  setInput("0")
+},[])
+
+
+
+const operatorType = (e)=> {
+  setTotal(false)
+  setOperator(e.target.innerText)
+  if(curState === "") return
+  if(preState !== ""){
+    equals()
+  }setPreState(curState)
+  setcurState("")
+}
+const equals = (e)=> {
+  if(e?.target.innerText === "="){
+  setTotal(true)
+  }
+let cal
+switch (operator) {
+  case "/":
+    cal = String(parseFloat(preState)/ parseFloat(curState)
+    )
+    break;
+  case "+":
+    cal = String(parseFloat(preState) + parseFloat(curState)
+    )
+    break
+  case "-":
+    cal = String(parseFloat(preState) - parseFloat(curState)
+      )
+      break;
+   case "x":
+        cal = String(parseFloat(preState) * parseFloat(curState)
+        )
+        break;
+  default:
+    return
+}
+setInput("")
+    setPreState(cal)
+    setcurState("")
+}
+const minutPlus = ()=> {
+  if(curState.charAt(0) == "-"){
+    setcurState(curState.substring(1))
+  }else{
+    setcurState("-"+curState)
+  }
+}
+const percent = ()=> {}
+const reset = () => {
+  setPreState("")
+  setcurState("")
+  setInput("")
+}
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='wrapper'>
+        <div className='screen'>{input !== "" || input == "0" ? 
+      <NumericFormat value={input} displayType={"text"} thousandSeperator={true}/> :
+      <NumericFormat value={preState} displayType={'text'} thousandSeparator={true} />  
+      }</div>
+        <div className='btn light-gray' onClick={reset}>AC</div>
+        <div className='btn light-gray' onClick={percent}>%</div>
+        <div className='btn light-gray' onClick={minutPlus}>+/-</div>
+        <div className='btn light-gray' onClick={operatorType}>/</div>
+        <div className='btn' onClick={inputNum}>7</div>
+        <div className='btn' onClick={inputNum}>8</div>
+        <div className='btn' onClick={inputNum}>9</div>
+        <div className='btn orange' onClick={operatorType}>X</div>
+        <div className='btn' onClick={inputNum}>4</div>
+        <div className='btn' onClick={inputNum}>5</div>
+        <div className='btn' onClick={inputNum}>6</div>
+        <div className='btn orange' onClick={operatorType}>+</div>
+        <div className='btn' onClick={inputNum}>3</div>
+        <div className='btn' onClick={inputNum}>2</div>
+        <div className='btn' onClick={inputNum}>1</div>
+        <div className='btn orange' onClick={operatorType}>-</div>
+        <div className='btn zero' onClick={inputNum}>0</div>
+        <div className='btn' onClick={inputNum}>.</div>
+        <div className='btn' onClick={equals}>=</div>
+      </div>
+     
     </div>
   );
 }
